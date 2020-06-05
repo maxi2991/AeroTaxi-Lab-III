3ab04f2d1855b6c9918e6b23a15e3e55cca2bb42
martinCaminero <martincaminero64@gmail.com>
2020-06-05 00:13:31 -0300

    termine la clase Archivo, fue una locura y no estoy del todo seguro de que hice, pero funciona. NUNCA MAS!
    Signed-off-by: martinCaminero <martincaminero64@gmail.com>

--- src/Clases/Usuario.java
+++ src/Clases/Usuario.java
@@ -1,6 +1,8 @@
 package Clases;
 
-public class Usuario {
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
     private String nombre;
     private String apellido;
     private int dni;
