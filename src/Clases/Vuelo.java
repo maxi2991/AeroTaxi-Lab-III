3bac03e1ec50638e64c530bd8683e3eacec7af75
maxi2991 <commissomaximiliano@gmail.com>
2020-06-04 23:09:41 -0300

    commit boolean vuelo

--- src/Clases/Vuelo.java
+++ src/Clases/Vuelo.java
@@ -1,15 +1,16 @@
 package Clases;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+
 
 public class Vuelo {
     private Avion transporte;
-    private SimpleDateFormat fecha = new SimpleDateFormat("dd-mm-yyyy");
+    private SimpleDateFormat fecha = new SimpleDateFormat("dd-MM-yyyy");
     private String origen;
     private String destino;
-    private List<Usuario> pasajeros = new ArrayList<>();
+    private boolean disponible; // disponibilidad del vuelo
+    private LinkedList<Usuario> pasajeros = new LinkedList<>();
 
     public Vuelo() {
 
@@ -20,6 +21,7 @@ public class Vuelo {
         this.fecha = fecha;
         this.origen = origen;
         this.destino = destino;
+        this.disponible = true;
 
     }
 
@@ -53,6 +55,14 @@ public class Vuelo {
 
     public void setDestino(String destino) {
         this.destino = destino;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
     }
 
     public void agregarPasajero(Usuario nuevo) {
@@ -89,10 +99,9 @@ public class Vuelo {
     public int contarPasajeros() {// Esto es por si algun pasajero cancela el vuelo , la celda de este objeto sera nula y no debe ser contada como un pasajero efectivo
         int validos = 0;
         for (int i = 0; i < pasajeros.size(); i++) {
-            if (pasajeros.get(i) != null) {
-                validos++;
-                validos += pasajeros.get(i).getAcompañantes(); //Agrego la cantidad de acompañantes del pasajero
-            }
+            validos++;
+            validos += pasajeros.get(i).getAcompañantes(); //Agrego la cantidad de acompañantes del pasajero
+
         }
 
         return validos;
@@ -140,15 +149,18 @@ public class Vuelo {
 
     public void mostrarPasajeros() {
         for (Usuario actual : this.pasajeros) {
-            if (actual != null) {
-                System.out.println(actual.toString());
-            }
+            System.out.println(actual);
+
         }
     }
 
     @Override
     public String toString() {
-        return this.transporte.toString() + " Salida: " + this.getFecha() + " Origen: " + this.getOrigen() + " Destino: " + this.getDestino();
+        if (this.disponible) {
+            return this.transporte.toString() + " Salida: " + this.getFecha() + " Origen: " + this.getOrigen() + " Destino: " + this.getDestino() + " Vuelo disponible";
+        } else {
+            return this.transporte.toString() + " Salida: " + this.getFecha() + " Origen: " + this.getOrigen() + " Destino: " + this.getDestino() + " Vuelo cancelado";
+        }
     }
 
 
